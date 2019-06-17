a96769451b638af39671579c6ce286e25177e5f0
Andy Rundquist <andy.rundquist@gmail.com>
2019-06-17 10:21:14 -0500

    capital PUT

--- resources/views/initiate.blade.php
+++ resources/views/initiate.blade.php
@@ -5,7 +5,7 @@
 
 
 @section('content')
-    <form action="{{{url('meeting/new')}}}" method="put">
+    <form action="{{{url('meeting/new')}}}" method="PUT">
       {{ csrf_field() }}
       Meeting name: <input type='text' name='meeting'><br/>
       your name: <input type='text' name='moderator'><br/>
